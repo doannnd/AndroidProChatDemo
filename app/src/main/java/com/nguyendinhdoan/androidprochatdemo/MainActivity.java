d9417fea3b9a64cf5cfec3bc6ac8af7c26b84161
doannd <nguyendinhdoanjdk@gmail.com>
2019-05-21 16:01:49 +0700

    sign up and sign in

--- app/src/main/java/com/nguyendinhdoan/androidprochatdemo/MainActivity.java
+++ app/src/main/java/com/nguyendinhdoan/androidprochatdemo/MainActivity.java
@@ -1,13 +1,79 @@
 package com.nguyendinhdoan.androidprochatdemo;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.quickblox.auth.session.QBSettings;
+import com.quickblox.core.QBEntityCallback;
+import com.quickblox.core.exception.QBResponseException;
+import com.quickblox.users.QBUsers;
+import com.quickblox.users.model.QBUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String APP_ID = "77088";
+    static final String AUTH_KEY = "8wsrhOhuDZmu4kd";
+    static final String AUTH_SECRET = "A5SUqQujWAZh5HC";
+    static final String ACCOUNT_KEY = "w_up-cko3qg8LYWYycBf";
+
+    private Button btnLogin, btnSignUp;
+    private TextInputEditText edtUser, edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeFrameWork();
+
+        btnLogin = findViewById(R.id.main_btnLogin);
+        btnSignUp = findViewById(R.id.main_btnSignUp);
+
+        edtUser = findViewById(R.id.main_editLogin);
+        edtPassword = findViewById(R.id.main_editPassword);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edtUser.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                QBUser qbUser = new QBUser(user, password);
+
+                QBUsers.signIn(qbUser).performAsync(
+                        new QBEntityCallback<QBUser>() {
+                            @Override
+                            public void onSuccess(QBUser qbUser, Bundle bundle) {
+                                Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onError(QBResponseException e) {
+                                Toast.makeText(MainActivity.this, "Login failed" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+            }
+        });
+
+
     }
+
+    private void initializeFrameWork() {
+        QBSettings.getInstance().init(getApplicationContext(), APP_ID, AUTH_KEY, AUTH_SECRET);
+        QBSettings.getInstance().setAccountKey(ACCOUNT_KEY);
+    }
+
 }
--- app/src/main/java/com/nguyendinhdoan/androidprochatdemo/SignUpActivity.java
+++ app/src/main/java/com/nguyendinhdoan/androidprochatdemo/SignUpActivity.java
@@ -1,13 +1,86 @@
 package com.nguyendinhdoan.androidprochatdemo;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.quickblox.auth.QBAuth;
+import com.quickblox.auth.session.QBSession;
+import com.quickblox.core.QBEntityCallback;
+import com.quickblox.core.exception.QBResponseException;
+import com.quickblox.users.QBUsers;
+import com.quickblox.users.model.QBUser;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
+
+    private Button btnSignUp, btnCancel;
+    private TextInputEditText edtUser, edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        registerSession();
+
+        btnSignUp = findViewById(R.id.signup_btnSignUp);
+        btnCancel = findViewById(R.id.signup_btnCancel);
+
+        edtUser = findViewById(R.id.signup_editLogin);
+        edtPassword = findViewById(R.id.signup_editPassword);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edtUser.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                QBUser qbUser = new QBUser(user, password);
+
+                QBUsers.signUp(qbUser).performAsync(
+                        new QBEntityCallback<QBUser>() {
+                            @Override
+                            public void onSuccess(QBUser qbUser, Bundle bundle) {
+                                Toast.makeText(SignUpActivity.this, "sign up successful", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+
+                            @Override
+                            public void onError(QBResponseException e) {
+                                Toast.makeText(SignUpActivity.this, "sign up failed" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+            }
+        });
+    }
+
+    private void registerSession() {
+        QBAuth.createSession().performAsync(
+                new QBEntityCallback<QBSession>() {
+                    @Override
+                    public void onSuccess(QBSession qbSession, Bundle bundle) {
+                        
+                    }
+
+                    @Override
+                    public void onError(QBResponseException e) {
+                        Log.e(TAG, "onError: " + e.getMessage());
+                    }
+                }
+        );
     }
 }
